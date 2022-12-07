308fce4fd3c9ab3bfec80f5d0185a4e08901e847
NathanPerry057 <G00394355@atu.ie>
2022-12-07 12:14:17 +0000

    Added getters and setters for both variables and gave them parameters they have to meet in the main public Employee

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,15 +1,41 @@
-package
+
 
 public class Employee {
 
+    public String name;
+    public String pps;
+
     public Employee(String name, String pps) {
 
+        if(name.length() >= 5 || name.length() <= 30) {
+            setname();
+        }
+        else {
+            throw new IllegalArgumentException("Invalid name, must be between 5 and 30 characters");
+        }
+
+        if(pps.length() == 6) {
+            setpps();
+        }
+
+        else {
+            throw new IllegalArgumentException("Employee PPS number should be 6 characters");
+        }
+
     }
 
 
-
-    void String getname(){
+    public String getname() {
         return name;
     }
+    public void setname() {
+        this.name = name;
+    }
 
+    public String getpps() {
+        return pps;
+    }
+    public void setpps() {
+        this.pps = pps;
+    }
 }
--- src/test/java/EmployeeList.java
+++ src/test/java/EmployeeList.java
@@ -1,2 +1,31 @@
+import org.junit.After;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class EmployeeList {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testname() {
+        Employee e = new Employee(String name, String pps);
+
+    }
+
+
+    @After
+    public void teardown() {
+
+    }
+
 }
+
+
